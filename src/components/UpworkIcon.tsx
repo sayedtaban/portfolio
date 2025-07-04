8d00ce9bf96ca83f6fd219acf06e49c235f5c790
sayedtaban <sayedtaban8@gmail.com>
2025-07-04 20:06:23 +0100

    update: upwork button.

--- src/components/UpworkIcon.tsx
+++ src/components/UpworkIcon.tsx
@@ -1,10 +1,9 @@
 // UpworkIcon.jsx or inline in your component
 const UpworkIcon = (props) => (
-    <svg viewBox="0 0 32 32" fill="currentColor" width="1em" height="1em" {...props}>
-      <g>
-        <path d="M27.5 6.5c-2.2 0-4.1 1.2-5.1 3.1l-2.2 4.2-2.2-4.2c-0.5-1-1.5-1.6-2.6-1.6-1.6 0-2.9 1.3-2.9 2.9v5.1c0 2.2-1.8 4-4 4s-4-1.8-4-4V8.5H2.5v7.5c0 4.1 3.4 7.5 7.5 7.5 2.2 0 4.1-1.2 5.1-3.1l2.2-4.2 2.2 4.2c0.5 1 1.5 1.6 2.6 1.6 1.6 0 2.9-1.3 2.9-2.9v-5.1c0-2.2 1.8-4 4-4s4 1.8 4 4v7.5h2.5v-7.5c0-4.1-3.4-7.5-7.5-7.5z"/>
-      </g>
-    </svg>
-  );
+  <svg viewBox="0 0 32 32" fill="none" width="1em" height="1em" {...props}>
+    <circle cx="16" cy="16" r="16" fill="#6fda44"/>
+    <text x="16" y="22" textAnchor="middle" fontSize="18" fontWeight="bold" fill="white" fontFamily="Arial, sans-serif">U</text>
+  </svg>
+);
 
 export default UpworkIcon;
